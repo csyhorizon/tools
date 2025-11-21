7a9dd56bf9a62433ba51952cb5d7c144f94ffe25
csy <tnehd83@naver.com>
2025-11-21 23:32:13 +0900

    fix:주석 일부 제거

--- k6/load-test.js
+++ k6/load-test.js
@@ -3,9 +3,9 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '10s', target: 10 },  // 처음 10초 동안 사용자 10명까지 서서히 증가
-    { duration: '30s', target: 50 },  // 30초 동안 사용자 50명까지 증가 (본격 부하)
-    { duration: '10s', target: 0 },   // 10초 동안 사용자 0명으로 감소 (정리)
+    { duration: '10s', target: 10 },
+    { duration: '30s', target: 50 },
+    { duration: '10s', target: 0 },
   ],
   thresholds: {
     http_req_duration: ['p(95)<500'],
